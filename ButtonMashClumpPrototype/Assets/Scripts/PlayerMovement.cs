653362d964881dbc6bccd7bf97d2d808fb66ecbb
BenSironko <bds371@nyu.edu>
2016-02-03 15:00:22 -0500

    Super Stoopid Player Movement Added

---
+++ ButtonMashClumpPrototype/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+	public float speed;
+
+	private Rigidbody2D rb2D;
+
+	private Player player;
+
+	void Start() {
+		player = GetComponent<Player>();
+		rb2D = GetComponent<Rigidbody2D>();
+	}
+
+	void Update() {
+		rb2D.velocity = (new Vector2(Input.GetAxisRaw("Horizontal" + player.number), Input.GetAxisRaw("Vertical" + player.number))).normalized * speed;
+	}
+}
