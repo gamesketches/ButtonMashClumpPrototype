b5c2f60f3bc174dd3f0e66eca328682588fdf949
gamesketches <sam.von.ehren@gmail.com>
2016-02-02 21:05:11 -0500

    First pass on the input interpreting script. There's no interpretation and it currently just reads if the buttons are pressed down which is super wrong but I'll change it in the next commit

---
+++ ButtonMashClumpPrototype/Assets/InputManager.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputManager : MonoBehaviour {
+
+	public string fireAxis;
+	public string buttonA;
+	public string buttonB;
+	public int mashBufferSize;
+
+	public int ShotsPerMinute;
+	private int shotCooldown;
+	private char[] mashBuffer;
+	private int bufferIter;
+
+	// Use this for initialization
+	void Start () {
+		// BPM to frames conversion is (60 / BPM) * F where F is frames per second
+		// Also: rounding simplicity
+		shotCooldown = Mathf.RoundToInt((60.0f / ShotsPerMinute) * 60.0f);
+		mashBuffer = new char[mashBufferSize];
+		bufferIter = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// This will probably be better as a GetKeyUp or Down
+		if(Input.GetAxis(fireAxis) != 0 && shotCooldown <= 0) {
+			InterpretInputs();
+			for(int i = 0; i < mashBufferSize; i++){
+				mashBuffer.SetValue('*', i);
+			}
+			bufferIter = 0;
+			return;
+		}
+		if(Input.GetAxis(buttonA) != 0) {
+			mashBuffer.SetValue('A', bufferIter);
+		}
+		else if(Input.GetAxis(buttonB) != 0) {
+			mashBuffer.SetValue('B', bufferIter);
+		}
+		// Hell yeah ternaries
+		bufferIter = bufferIter >= mashBufferSize - 1 ? 0 : bufferIter + 1;
+		shotCooldown--;
+	}
+
+	void InterpretInputs() {
+		// Do some stuff here
+		for(int i = 0; i < mashBufferSize; i++){
+			Debug.Log(mashBuffer[i]);
+		}
+	}
+}
