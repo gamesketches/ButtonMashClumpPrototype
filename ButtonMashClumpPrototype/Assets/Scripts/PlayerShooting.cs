a23b1cb702b2e853a676663ebd1923ea57ed0d28
BenSironko <bds371@nyu.edu>
2016-02-04 00:46:15 -0500

    Added Rudimentary Shooting Functionality
    Look in the PlayerShooting component on the Player object. It contains
    a public method called Shoot that takes in a bullet type (in the form
    of an integer), the angle in degrees that the bullet should travel, and
    the speed at which the bullet should travel.

---
+++ ButtonMashClumpPrototype/Assets/Scripts/PlayerShooting.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerShooting : MonoBehaviour {
+	public GameObject basicBulletPrefab;
+
+	void Update() {
+		if(Input.GetKeyDown(KeyCode.Space)) {
+			Shoot(0, 0.0f, 20.0f);
+		}
+	}
+
+	public void Shoot(int bulletType, float angleInDegrees, float speed) {
+		Rigidbody2D bullet;
+		//if(bulletType == 0) {
+			bullet = ((GameObject)Instantiate (basicBulletPrefab, transform.position, Quaternion.Euler (0.0f, 0.0f, angleInDegrees))).GetComponent<Rigidbody2D> ();
+		//}
+		bullet.velocity = new Vector2(Mathf.Cos(angleInDegrees * Mathf.Deg2Rad), Mathf.Sin(angleInDegrees * Mathf.Deg2Rad)) * speed;
+	}
+}
