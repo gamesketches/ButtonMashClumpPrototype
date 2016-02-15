5132a82e2cf17d4f2c2edf9876b85ec213e07ec6
gamesketches <sam.von.ehren@gmail.com>
2016-02-15 01:56:16 -0500

    Blood prefab changes color before disappearing

--- ButtonMashClumpPrototype/Assets/BloodScript.cs
+++ ButtonMashClumpPrototype/Assets/BloodScript.cs
@@ -4,15 +4,24 @@ using System.Collections;
 public class BloodScript : MonoBehaviour {
 
 	public int framesOfExistence;
+	public Color startColor;
+	public Color endColor;
+	private Renderer quadColor;
+	private int startFrames;
 	// Use this for initialization
 	void Start () {
-	
+		quadColor = gameObject.GetComponentsInChildren<Renderer>()[0];
+		startFrames = framesOfExistence;
+		startColor.a = 1.0f;
+		endColor.a = 1.0f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if(framesOfExistence > 0 ) {
 			framesOfExistence--;
+			quadColor.material.color = Color.Lerp(endColor, startColor, 
+										(float)framesOfExistence / (float)startFrames);
 		}
 		else {
 			Destroy(gameObject);
