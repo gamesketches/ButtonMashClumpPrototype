35e42424650fd0063698b5eee435053f32c36812
gamesketches <sam.von.ehren@gmail.com>
2016-02-15 01:41:50 -0500

    Added blood prefab. Just destroys itself after a publicly defined number of frames. Probably add some color change code next

---
+++ ButtonMashClumpPrototype/Assets/BloodScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BloodScript : MonoBehaviour {
+
+	public int framesOfExistence;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(framesOfExistence > 0 ) {
+			framesOfExistence--;
+		}
+		else {
+			Destroy(gameObject);
+		}
+	}
+}
