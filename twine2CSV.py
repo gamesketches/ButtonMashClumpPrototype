fb0f3635ec21407cdaf5627c39ad585c79ed3380
gamesketches <sam.von.ehren@gmail.com>
2016-03-21 23:03:35 -0400

    Created a python script to convert twine to jsons. Probably undo some of my data structure from before since we can just read it as Json from here on out

---
+++ twine2CSV.py
@@ -0,0 +1,45 @@
+import os, json
+
+def twine2CSV():#filename, outputFilename):
+    input = open("Kopesh_story_draft.html", "r")
+    fullText = input.readlines()
+    fullText = fullText[364]
+    input.close()
+    allLines = fullText.split('<')[2:]
+    data = {}
+    for curLine in allLines:
+        if curLine == '/div>':
+            continue
+        firstQuotation = curLine.find('\"') + 1
+        if firstQuotation == 0:
+            continue
+        key = curLine[firstQuotation : firstQuotation + curLine[firstQuotation:].index('\"') ]
+        if key == "StoryAuthor":
+            continue
+        sentence = curLine[curLine.index('>') + 1 : ]
+        scriptLines = sentence.split('.')
+        fullySplitLines = []
+        for line in scriptLines:
+            fullySplitLines += line.split('\\n')
+
+        fullySplitLines = [x for x in fullySplitLines if x != ""]
+        horusKey = "gameOver"
+        setKey = "gameOver"
+        print fullySplitLines
+        if fullySplitLines[-2].find("|") > 0:
+            pipeIndex = fullySplitLines[-2].find("|") + 1
+            horusKey = fullySplitLines[-2][pipeIndex : fullySplitLines[-2].find("]")]
+            print horusKey
+            pipeIndex = fullySplitLines[-1].find("|") + 1
+            setKey = fullySplitLines[-1][pipeIndex : fullySplitLines[-1].find("]")]
+            print setKey
+            fullySplitLines = fullySplitLines[:-2]
+
+        data[key] = {"lines":fullySplitLines, "horusKey":horusKey, "setKey":setKey}
+
+    data_string = json.dumps([data], indent=2)
+    outputFile = open("output.json", "w")
+    outputFile.write(data_string)
+
+
+twine2CSV()
