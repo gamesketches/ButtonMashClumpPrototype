8f1cd96a280b8b49050aedc3e76368ee27f34ac3
BenSironko <bds371@nyu.edu>
2016-03-31 00:00:50 -0400

    Adapted Ben-Style Aiming System to Reticle

--- ButtonMashClumpPrototype/Assets/Scripts/PlayerMovement.cs
+++ ButtonMashClumpPrototype/Assets/Scripts/PlayerMovement.cs
@@ -33,12 +33,10 @@ public class PlayerMovement : MonoBehaviour {
 		player = GetComponent<Player>();
 		rb2D = GetComponent<Rigidbody2D>();
 		anim = GetComponent<Animator>();
-		//reticleRenderer = GetComponent<SpriteRenderer>();
 		reticleRenderer.color = reticleGradient.Evaluate(0.0f);
 	}
 
 	void Update() {
-		//reticleRenderer.color = reticleGradient.Evaluate(
 		HandleMovement();
 	}
 
@@ -58,16 +56,21 @@ public class PlayerMovement : MonoBehaviour {
 				anim.SetTrigger("Walk South");
 			}
 		}
-		reticle.transform.localPosition = new Vector3(Mathf.Cos(radians) * reticleRadius, Mathf.Sin(radians) * reticleRadius, 0.0f);
-		reticle.transform.localRotation = Quaternion.Euler(0.0f, 0.0f, degrees - 90.0f);
-		reticleBlast.transform.localPosition = new Vector3(Mathf.Cos(radians) * reticleBlastRadius, Mathf.Sin(radians) * reticleBlastRadius, 0.0f);
-		reticleBlast.transform.localRotation = reticle.transform.localRotation;
+		if(!useBenAiming) {
+			reticle.transform.localPosition = new Vector3(Mathf.Cos(radians) * reticleRadius, Mathf.Sin(radians) * reticleRadius, 0.0f);
+			reticle.transform.localRotation = Quaternion.Euler(0.0f, 0.0f, degrees - 90.0f);
+			reticleBlast.transform.localPosition = new Vector3(Mathf.Cos(radians) * reticleBlastRadius, Mathf.Sin(radians) * reticleBlastRadius, 0.0f);
+			reticleBlast.transform.localRotation = reticle.transform.localRotation;
+		}
 	}
 
 	public void HandleRotation() {
 		float targetAngle = Mathf.Atan2(opponent.transform.position.y - transform.position.y, opponent.transform.position.x - transform.position.x) * Mathf.Rad2Deg + player.number * 180.0f;
 		currentAngle = Mathf.LerpAngle(currentAngle, targetAngle, rotateSpeed * Time.deltaTime);
-		transform.rotation = Quaternion.Euler(0.0f, 0.0f, currentAngle);
+		reticle.transform.localPosition = new Vector3(Mathf.Cos((currentAngle + 180.0f * player.number) * Mathf.Deg2Rad) * reticleRadius, Mathf.Sin((currentAngle + 180.0f * player.number) * Mathf.Deg2Rad) * reticleRadius, 0.0f);
+		reticle.transform.localRotation = Quaternion.Euler(0.0f, 0.0f, (currentAngle + 180.0f * player.number) - 90.0f);
+		reticleBlast.transform.localPosition = new Vector3(Mathf.Cos((currentAngle + 180.0f * player.number) * Mathf.Deg2Rad) * reticleBlastRadius, Mathf.Sin((currentAngle + 180.0f * player.number) * Mathf.Deg2Rad) * reticleBlastRadius, 0.0f);
+		reticleBlast.transform.localRotation = reticle.transform.localRotation;
 	}
 
 	public void PassBufferToReticle(int bufferIndex, int mashBufferSize) {
@@ -80,7 +83,7 @@ public class PlayerMovement : MonoBehaviour {
 
 	public float currentShotAngle() {
 		if(useBenAiming) {
-			return transform.rotation.eulerAngles.z + 180.0f * player.number;
+			return currentAngle + 180.0f * player.number;
 		} else {
 			float radians = Mathf.Atan2(lastMovement.y, lastMovement.x);
 			return radians * Mathf.Rad2Deg;
