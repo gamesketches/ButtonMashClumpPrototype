cb7c91d39e97330d2e0ba5c076f410daeb69c900
skisonic <skisonic@gmail.com>
2016-03-31 16:16:14 -0400

    Added visual feedback to bullet collison

---
+++ ButtonMashClumpPrototype/Assets/Scripts/BulletCollisionScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletCollisionScript : MonoBehaviour {
+
+	public int framesOfExistence;
+	public Color startColor;
+	public Color endColor;
+	private Renderer quadColor;
+	private int startFrames;
+
+    
+	// Use this for initialization
+	void Start () {
+		quadColor = gameObject.GetComponentsInChildren<Renderer>()[0];
+		startFrames = framesOfExistence;
+		startColor.a = 1.0f;
+		endColor.a = 1.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(framesOfExistence > 0 ) {
+			framesOfExistence--;
+			//quadColor.material.color = Color.Lerp(endColor, startColor, 
+				//						(float)framesOfExistence / (float)startFrames);
+		}
+		else {
+			Destroy(gameObject);
+		}
+	}
+}
